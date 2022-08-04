5e7c30d8a6d599b57bf0609e12c586056ce2306a
russell-leung <russell-leung@comcast.net>
2022-08-04 11:19:44 -0400

    workign api

---
+++ app/src/Controller/ArticlesController.php
@@ -0,0 +1,115 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Articles Controller
+ *
+ */
+class ArticlesController extends AppController
+{
+    public function viewClasses(): array {
+        return [JsonView::class];
+    }
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index() {
+        $articles = $this->Articles->find('all')->all();
+        $this->set('articles', $articles);
+        $this->viewBuilder()->setOption('serialize', ['articles']);
+        $this->set(compact('articles'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Article id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null) {
+
+        $article = $this->Articles->get($id);
+        $this->set('article', $article);
+        $this->viewBuilder()->setOption('serialize', ['article']);
+
+        $article = $this->Articles->get($id, [
+            'contain' => [],
+        ]);
+
+        $this->set(compact('article'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add() {
+        if ($this->request->is('post')) {
+            $article = $this->Articles->newEntity($this->request->getData());
+            if ($this->Articles->save($article)) {
+                $message = 'Saved';
+            } else {
+                $message = ['Error', $article->getErrors()];
+            }
+            $this->set([
+                'message' => $message,
+                'article' => $article,
+            ]);
+        }
+        $this->viewBuilder()->setOption('serialize', ['article', 'message']);
+
+        $this->set(compact('article'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Article id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null) {
+
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $article = $this->Articles->get($id);
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $message = 'Saved';
+            } else {
+                $message = 'Error';
+            }
+            $this->set([
+                'message' => $message,
+                'article' => $article,
+            ]);
+            $this->viewBuilder()->setOption('serialize', ['article', 'message']);
+        }
+        $this->set(compact('article'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Article id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null) {
+
+        $this->request->allowMethod(['delete']);
+        $article = $this->Articles->get($id);
+        $message = 'Deleted';
+        if (!$this->Articles->delete($article)) {
+            $message = 'Error';
+        }
+        $this->set('message', $message);
+        $this->viewBuilder()->setOption('serialize', ['message']);
+    }
+}
